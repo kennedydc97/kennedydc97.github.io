a93d9cbd77900b7f19073ef74fa0597e8329c375
Kennedy <kennedy_dourado@hotmail.com>
2023-05-13 20:00:32 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,105 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'beercommerce';
+  items = [
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-350.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-350.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-350.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-350.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+    {
+      title: 'Beck 350ml',
+      subtitle: 'Subtitle 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.',
+      image: 'assets/beck-330.jpg'
+    },
+  ];
 }
